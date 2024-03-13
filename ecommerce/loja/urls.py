2518270fe743626ad41a5d4d8fc5af2a81f362f3
Rhaony Ferraz <rhaonyferraz@hotmail.com>
2024-03-13 19:36:19 -0300

    links
    links

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', homepage, name="homepage")
+    path('', homepage, name="homepage"),
+    path('loja/', loja, name="loja"),
+    path('minhaconta/', minha_conta, name="minha_conta"),
+    path('login', login, name="login"),
+    path('carrinho', carrinho, name="carrinho"),
+    path('checkout', checkout, name="checkout"),
 ]
--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def loja(request):
+    return render(request, 'loja.html')
+
+
+def minha_conta(request):
+    return render(request, 'usuario/minha_conta.html')
+
+
+def login(request):
+    return render(request, 'usuario/login.html')
+
+
+def carrinho(request):
+    return render(request, 'carrinho.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
