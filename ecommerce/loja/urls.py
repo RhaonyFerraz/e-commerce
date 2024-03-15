4c8e2d95f68bc5dde8f01ae48abeac572c07ee5e
Rhaony Ferraz <rhaonyferraz@hotmail.com>
2024-03-15 00:23:37 -0300

    base html
    ligação simplificada %%

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', homepage, name="homepage"),
     path('loja/', loja, name="loja"),
     path('minhaconta/', minha_conta, name="minha_conta"),
-    path('login', login, name="login"),
-    path('carrinho', carrinho, name="carrinho"),
-    path('checkout', checkout, name="checkout"),
+    path('login/', login, name="login"),
+    path('carrinho/', carrinho, name="carrinho"),
+    path('checkout/', checkout, name="checkout"),
 ]
