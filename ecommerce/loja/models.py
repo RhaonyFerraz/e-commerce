c0c2fbb99f0b8aee7517ef3d6c9c5eaf35164b6e
Rhaony Ferraz <rhaonyferraz@hotmail.com>
2024-03-17 10:31:38 -0300

    banco de dados
    banco de dados  finalizado

--- ecommerce/loja/models.py
+++ ecommerce/loja/models.py
@@ -5,18 +5,69 @@ from django.contrib.auth.models import User
 
 
 class Cliente(models.Model):
-    nome = models.CharField(max_Length=200, nuLL=True, bLank=True)
-    email = models.CharField(max_Length=200, nuLL=True, bLank=True)
-    telefone = models.CharField(max_Length=200, nuLL=True, bLank=True)
-    id_sessao = models.CharField(max_Length=200, nuLL=True, bLank=True)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    telefone = models.CharField(max_length=200, null=True, blank=True)
+    id_sessao = models.CharField(max_length=200, null=True, blank=True)
     usuario = models.OneToOneField(
-        User, nuLL=True, bLank=True, on_deLete=models.CASCADE)
+        User, null=True, blank=True, on_delete=models.CASCADE)
 
-# _Cliente
-# nome
-# email
-# telefone
-# usuario
+
+class Categoria(models.Model):  # Categorias (Masculino, Feminino, Infantil)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+
+
+class Tipo(models.Model):  # Tipos (Camisa, Camiseta, Bermuda, Calça)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+
+
+class Produto(models.Model):
+    imagem = models.CharField(max_length=400, null=True, blank=True)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    ativo = models.BooleanField(default=True)
+    categoria = models.ForeignKey(
+        Categoria, null=True, blank=True, on_delete=models.SET_NULL)
+    tipo = models.ForeignKey(
+        Tipo, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class ItemEstoque(models.Model):
+    produto = models.ForeignKey(
+        Produto, null=True, blank=True, on_delete=models.SET_NULL)
+    cor = models.CharField(max_length=200, null=True, blank=True)
+    tamanho = models.CharField(max_length=200, null=True, blank=True)
+    quantidade = models.IntegerField(default=0)
+
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=400, null=True, blank=True)
+    numero = models.IntegerField(default=0)
+    complemento = models.CharField(max_length=200, null=True, blank=True)
+    cep = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=200, null=True, blank=True)
+    estado = models.CharField(max_length=200, null=True, blank=True)
+    cliente = models.ForeignKey(
+        Cliente, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(
+        Cliente, null=True, blank=True, on_delete=models.SET_NULL)
+    finalizado = models.BooleanField(default=False)
+    codigo_transacao = models.CharField(max_length=200, null=True, blank=True)
+    endereco = models.ForeignKey(
+        Endereco, null=True, blank=True, on_delete=models.SET_NULL)
+    data_finalizacao = models.DateTimeField(null=True, blank=True)
+
+
+class ItensPedido(models.Model):
+    item_estoque = models.ForeignKey(
+        ItemEstoque, null=True, blank=True, on_delete=models.SET_NULL)
+    quantidade = models.IntegerField(default=0)
+    pedido = models.ForeignKey(
+        Pedido, null=True, blank=True, on_delete=models.SET_NULL)
+
 
 # _produto
 # imagem
