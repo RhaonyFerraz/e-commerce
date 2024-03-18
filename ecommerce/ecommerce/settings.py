8bec6b1a327ac96d4f8933c1946b0a4454a2c255
Rhaony Ferraz <rhaonyferraz@hotmail.com>
2024-03-18 10:55:28 -0300

    media no arquivo settings
    fixano gestao de imagens

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MIDIA_URL = "imagens/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
