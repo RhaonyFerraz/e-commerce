642a907cf8d25baafa3f2a4c2c513cdc9cd78f0b
Rhaony Ferraz <rhaonyferraz@hotmail.com>
2024-03-23 02:19:51 -0300

    atualizando e corrigindo
    item de entrada

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -50,14 +50,29 @@ def login(request):
 def adicionar_carrinho(request, id_produto):
     if request.method == "POST" and id_produto:
         dados = request.POST.dict()
-        print(dados)
         tamanho = dados.get("tamanho")
         id_cor = dados.get("cor")
         if not tamanho:
             return redirect('loja')
+        if request.user.is_authenticated:
+            cliente = request.user.cliente
+        else:
+            return redirect('loja')
+        pedido, criado = Pedido.objects.get_or_create(
+            cliente=cliente, finalizado=False)
+        item_estoque = ItemEstoque.objects.get(
+            produto__id=id_produto, tamanho=tamanho, cor__id=id_cor)
+        item_pedido, criado = ItensPedido.objects.get_or_create(
+            item_estoque=item_estoque, pedido=pedido)
+        item_pedido.quantidade += 1
+        item_pedido.save()
         return redirect('carrinho')
     else:
         return redirect('loja')
+
+
+def remover_carrinho(request):
+    return redirect('carrinho')
 
 
 def carrinho(request):
